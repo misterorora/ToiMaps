73a98b25b04f2f6c93b8b29b92290df056957256
ChristianSteiner <cs.christian.steiner@gmail.com>
2023-09-14 12:11:58 +0200

    added template

--- ToiMaps/src/databaseHandler.go
+++ ToiMaps/src/databaseHandler.go
@@ -1,5 +1,12 @@
 package main
 
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
+
 const (
 	host     = "85.214.35.122"
 	port     = 5432
@@ -7,6 +14,24 @@ const (
 	password = "toimap"
 	dbname   = "toimapsdb"
 )
+
+func main() {
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Successfully connected!")
+}
 
 type DatabaseClient struct {
 }
