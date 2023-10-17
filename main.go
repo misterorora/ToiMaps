bb5c7cc8150bc941ea3ba20d87f2cfa6cff4f0f1
MuffinCPT <benjamin.liebenberg04@gmail.com>
2023-10-17 14:57:00 +0200

    changed html to gohtml and implemented the whole backend to frontend communication

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ func main() {
 	router.Use(gin.Logger(), gin.Recovery())
 	router.SetHTMLTemplate(Tpl)
 	router.UseRawPath = true
+	router.Static("/visual", "./visual")
 
 	handler := Handler{
 		Name: "Handler",
@@ -23,18 +24,3 @@ func main() {
 
 	router.Run(":8080") //listen and serve on 0.0.0.0:8080
 }
-
-/*
-{{range $marker := .marker .}}
-    // L.marker is a low-level marker constructor in Leaflet.
-    var marker = L.marker([{{$marker.latitude}}, {{$marker.longitude}}], {
-        icon: L.mapbox.marker.icon({
-            'marker-size': 'large',
-            'marker-symbol': 'toilet',
-            'marker-color': '#fa0'
-        })
-    }).addTo(map);
-    {{end}}
-
-
-*/
